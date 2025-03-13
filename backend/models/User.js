cf8f174c1e12d84b87db83e7633f4f4427876613
Halit Çelenk <93710943+hcelenkk@users.noreply.github.com>
2025-03-13 10:32:30 +0300

    Delete backend directory

--- backend/models/User.js
+++
@@ -1,7 +0,0 @@
-const userSchema = new mongoose.Schema({
-    tcKimlik: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ["aday", "admin", "yonetici", "jüri"], required: true },
-    email: { type: String, required: true },
-    // Diğer alanlar (ad, soyad, vs.)
-  });
--- backend/utils/PdfGenerator.js
+++
@@ -1,9 +0,0 @@
-// utils/pdfGenerator.js
-const PDFDocument = require("pdfkit");
-const generatePDF = (basvuru) => {
-  const doc = new PDFDocument();
-  doc.text(`Başvuru ID: ${basvuru._id}`);
-  doc.text(`Aday: ${basvuru.adayID.ad}`);
-  // Tablo 5 içeriği...
-  return doc;
-};
