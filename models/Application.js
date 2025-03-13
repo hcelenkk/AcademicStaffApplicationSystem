81dc78988072355964334d540b5607e4aae04eda
Halit Çelenk <93710943+hcelenkk@users.noreply.github.com>
2025-03-13 10:32:18 +0300

    Delete models directory

--- models/Application.js
+++
@@ -1,6 +0,0 @@
-const basvuruSchema = new mongoose.Schema({
-    adayID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    ilanID: { type: mongoose.Schema.Types.ObjectId, ref: "Ilan" },
-    belgeler: [String], // AWS S3 URL'leri
-    durum: { type: String, enum: ["Beklemede", "Onaylandı", "Reddedildi"] },
-  });
